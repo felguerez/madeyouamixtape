bee5fe031bc3d08dbe726fe17a40f0070d450e0b
Antonio Felguerez <antoniof@spotify.com>
2020-05-01 07:55:00 -0400

    Add mysql db connection, constants

---
+++ lib/constants.ts
@@ -0,0 +1,11 @@
+export const AUTH_ENDPOINT: string = "https://accounts.spotify.com/authorize/";
+export const REDIRECT_URI: string =
+  process.env.NODE_ENV === "production"
+    ? "https://yf-playlist.now.sh/api/callback"
+    : "http://localhost:3000/api/callback";
+export const SCOPES: string[] = [
+  "user-read-currently-playing",
+  "user-read-playback-state",
+  "playlist-modify-private",
+];
+export const PLAYLIST_ID = "3qr5qFKyiGDniMIgJ45mix";
---
+++ lib/db/index.ts
@@ -0,0 +1,21 @@
+const mysql = require("serverless-mysql");
+
+console.log("process.env.MYSQL_HOST:", process.env.MYSQL_HOST);
+export const db = mysql({
+  config: {
+    host: process.env.MYSQL_HOST,
+    database: process.env.MYSQL_DATABASE,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+  },
+});
+
+export const query = async (query) => {
+  try {
+    const results = await db.query(query);
+    await db.end();
+    return results;
+  } catch (error) {
+    return { error };
+  }
+};
