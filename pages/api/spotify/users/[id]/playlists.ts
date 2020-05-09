1b4f10e899c9e34f269269b897c60819d73a94d5
Antonio Felguerez <antoniof@spotify.com>
2020-05-09 17:23:22 -0400

    Catch error in playlists

--- pages/api/spotify/users/[id]/playlists.ts
+++ pages/api/spotify/users/[id]/playlists.ts
@@ -1,15 +1,20 @@
 import { SPOTIFY_API_BASE } from "../../../../../lib/constants";
 
 export default async (req, res) => {
-  const playlists = await fetch(
-    `${SPOTIFY_API_BASE}users/${req.query.id}/playlists`,
-    {
-      headers: {
-        "Access-Control-Allow-Headers": "*",
-        Authorization: `Bearer ${req.query.token}`,
-      },
-    }
-  );
-  const json = await playlists.json();
-  res.status(200).json(json);
+  console.log("req.query.token in playlist:", req.query.token);
+  try {
+    const playlists = await fetch(
+      `${SPOTIFY_API_BASE}users/${req.query.id}/playlists`,
+      {
+        headers: {
+          "Access-Control-Allow-Headers": "*",
+          Authorization: `Bearer ${req.query.token}`,
+        },
+      }
+    );
+    const json = await playlists.json();
+    res.status(200).json(json);
+  } catch (e) {
+    console.log("Error in playlists ", e);
+  }
 };
