a88f65e044af75cff1eb5e42ed9a2de769228e20
Antonio Felguerez <antoniof@spotify.com>
2020-05-09 12:19:16 -0400

    log swaps on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 /** @jsx jsx */
 import Layout from "../components/layout";
 import { css, jsx } from "@emotion/core";
+import * as db from "../lib/db";
+import escape from "sql-template-strings";
 
-const Home = () => {
+const Home = ({ swaps }) => {
+  console.log("swaps:", swaps);
   return (
     <Layout>
       <h1
@@ -19,4 +22,16 @@ const Home = () => {
   );
 };
 
+export async function getServerSideProps({ req }) {
+  const [swaps] = await db.query(escape`
+        SELECT *
+        FROM swap
+      `);
+
+  return {
+    props: {
+      swaps: JSON.stringify(swaps),
+    },
+  };
+}
 export default Home;
