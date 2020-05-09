2fd0384aa7b19bfaa0c1756addbf31b4f8ee4f87
Antonio Felguerez <antoniof@spotify.com>
2020-05-09 12:21:31 -0400

    We don't need swaps on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,8 @@
 /** @jsx jsx */
 import Layout from "../components/layout";
 import { css, jsx } from "@emotion/core";
-import * as db from "../lib/db";
-import escape from "sql-template-strings";
 
-const Home = ({ swaps }) => {
-  console.log("swaps:", swaps);
+const Home = () => {
   return (
     <Layout>
       <h1
@@ -22,16 +19,4 @@ const Home = ({ swaps }) => {
   );
 };
 
-export async function getServerSideProps({ req }) {
-  const [swaps] = await db.query(escape`
-        SELECT *
-        FROM swap
-      `);
-
-  return {
-    props: {
-      swaps: JSON.stringify(swaps),
-    },
-  };
-}
 export default Home;
