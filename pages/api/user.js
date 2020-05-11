d1fa435d8f0a997597be7dd105061ec9d5818ce0
Antonio Felguerez <antoniof@spotify.com>
2020-05-11 11:26:48 -0400

    Add request log

--- pages/api/user.js
+++ pages/api/user.js
@@ -14,6 +14,7 @@ export default async function user(req, res) {
       .json({ user: user || null, spotifyUser: spotifyUser || null });
   } catch (e) {
     console.log("e:", e);
+    console.log("req:", req);
     res.status(500).json({ error: e });
   }
 }
