37dffac6a7d6938f93765f044976e9d403979523
Antonio Felguerez <antoniof@spotify.com>
2020-05-11 11:16:26 -0400

    Catch error in /api/auth/spotify

--- pages/api/auth/spotify.js
+++ pages/api/auth/spotify.js
@@ -5,7 +5,12 @@ import { authenticate } from "../../../lib/passport-authenticate";
 export default nextConnect()
   .use(passport.initialize())
   .get(async (req, res) => {
-    authenticate("spotify", req, res).then(() => {
-      res.status(200).json({});
-    });
+    authenticate("spotify", req, res)
+      .then(() => {
+        res.status(200).json({});
+      })
+      .catch((e) => {
+        console.log("e:", e);
+        res.status(500).json({ error: e });
+      });
   });
