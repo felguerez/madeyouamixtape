a99e33c43253a9e771bfaa9581dca2a7a4a4825e
Antonio Felguerez <antoniof@spotify.com>
2020-05-15 09:10:47 -0400

    Create selected_playlist endpoint

--- components/swaps/SelectedPlaylist.tsx
+++ components/swaps/SelectedPlaylist.tsx
@@ -4,11 +4,15 @@ import styled from "@emotion/styled";
 import { useSwapDispatch, useSwapState } from "../../contexts/swap-context";
 import { ContentCard } from "../../shared/styles";
 import { ButtonLink } from "../SwapManager";
+import { useRouter } from "next/router";
+import * as constants from "../../lib/constants";
+import Link from "next/link";
 
 export const SelectedPlaylist = ({
   currentSwapMember: { selected_playlist_id },
 }) => {
   const dispatch = useSwapDispatch();
+  const router = useRouter();
   const { selectedPlaylist, selectedPlaylistId } = useSwapState();
   useEffect(() => {
     if (!selectedPlaylistId) {
@@ -33,55 +37,107 @@ export const SelectedPlaylist = ({
       fetchData().catch((err) => {});
     }
   }, [selectedPlaylistId]);
+  if (!selectedPlaylist)
+    return (
+      <BodyContent>
+        <h2>Loading...</h2>
+      </BodyContent>
+    );
+  if (router.pathname.includes("selected_playlist")) {
+    return (
+      <BodyContent>
+        <p>playlist goes here</p>
+      </BodyContent>
+    );
+  }
   console.log("selectedPlaylist:", selectedPlaylist);
   return (
     <BodyContent>
-      {selectedPlaylist ? (
-        <Container>
-          <h2>Selection</h2>
-          <p>
-            Don't like this one?{" "}
-            <ButtonLink
-              onClick={() =>
-                dispatch({
-                  type: "SET_PLAYLIST_VIEWER",
-                  playlistViewer: "selector",
-                })
-              }
-            >
-              Choose a new playlist to share
-            </ButtonLink>
-          </p>
-          <ContentCard>
-            {selectedPlaylist.images?.length && (
-              <CoverArt src={selectedPlaylist.images[0].url} />
-            )}
-            <Metadata>
-              <Copy>
-                <PlaylistName>{selectedPlaylist.name}</PlaylistName>
-                <Description
-                  dangerouslySetInnerHTML={{
-                    __html: selectedPlaylist.description,
-                  }}
-                />
-              </Copy>
-              <Creator>
-                By{" "}
-                <a href={selectedPlaylist.owner.href} target="_blank">
-                  {selectedPlaylist.owner.display_name}
-                </a>
-              </Creator>
-              <TracksCount>
-                {selectedPlaylist.tracks.items.length} tracks
-              </TracksCount>
-            </Metadata>
-          </ContentCard>
-        </Container>
-      ) : (
-        <Container>
-          <h2>Loading...</h2>
-        </Container>
-      )}
+      <Container>
+        <h2>Selection</h2>
+        <p>
+          This playlist will be shared with someone else in your group. You'll
+          get a new playlist in return.
+        </p>
+        <p>
+          Don't like this one?{" "}
+          <ButtonLink
+            onClick={() =>
+              dispatch({
+                type: "SET_PLAYLIST_VIEWER",
+                playlistViewer: "selector",
+              })
+            }
+          >
+            Choose a new playlist to share
+          </ButtonLink>
+        </p>
+        <ContentCard>
+          {selectedPlaylist.images?.length && (
+            <CoverArt src={selectedPlaylist.images[0].url} />
+          )}
+          <Metadata>
+            <Copy>
+              <PlaylistName
+                onClick={async () => {
+                  try {
+                    const playlist = await fetch(
+                      `/api/swaps/${router.query.id}/selected_playlist?selectedPlaylistId=${selectedPlaylist.id}`
+                    );
+                    const json = playlist.json();
+                    console.log("json():", json());
+                  } catch (e) {
+                    console.log("e:", e);
+                  }
+                }}
+              >
+                {selectedPlaylist.name}
+              </PlaylistName>
+              <Description
+                dangerouslySetInnerHTML={{
+                  __html: selectedPlaylist.description,
+                }}
+              />
+            </Copy>
+            <Creator>
+              By{" "}
+              <a
+                href={selectedPlaylist.owner.href}
+                target="_blank"
+                onClick={async (e) => {
+                  e.preventDefault();
+                  try {
+                    const apiUserProfile = await fetch(
+                      `${constants.SPOTIFY_API_BASE}users/${selectedPlaylist.owner.id}`,
+                      {
+                        headers: {
+                          Authorization: `Basic ${Buffer.from(
+                            process.env.CLIENT_ID +
+                              ":" +
+                              process.env.CLIENT_SECRET
+                          ).toString("base64")}`,
+                          Accept:
+                            "application/json, application/xml, text/play, text/html, *.*",
+                          "Content-Type":
+                            "application/x-www-form-urlencoded; charset=utf-8",
+                        },
+                      }
+                    );
+                    apiUserProfile.json();
+                  } catch (e) {
+                    console.log("e:", e);
+                  }
+                }}
+              >
+                {selectedPlaylist.owner.display_name}
+              </a>
+            </Creator>
+            <TracksCount>
+              {selectedPlaylist.tracks.items.length} tracks
+            </TracksCount>
+          </Metadata>
+        </ContentCard>
+      </Container>
     </BodyContent>
   );
 };
