a3c30dbb5ccf691d7f617313cac57f5ac03c2844
Antonio Felguerez <antoniof@spotify.com>
2020-05-15 09:36:17 -0400

    Add selected_playlist route

--- pages/swaps/[id]/selected_playlist.tsx
+++ pages/swaps/[id]/selected_playlist.tsx
@@ -12,19 +12,43 @@ const SelectedPlaylist = () => {
       ).catch((err) => {
         console.log("err:", err);
       });
-      const { playlist } = await request.json();
-      setPlaylist(playlist);
+      const response = await request.json();
+      setPlaylist(response.playlist);
     }
     if (selectedPlaylistId) {
       fetchData().catch((err) => {});
     }
   }, [selectedPlaylistId]);
+  if (!playlist) {
+    return <h2>Loading...</h2>;
+  }
   return (
     <>
-      <h2>Selected playlist</h2>
+      <h2>{playlist.name}</h2>
       <p>This is the playlist you chose. Looks great!</p>
       <div>
         <h3>{playlist?.name}</h3>
+        <ul>
+          {playlist.tracks &&
+            playlist.tracks.items.map((item) => {
+              return (
+                <li>
+                  <p>
+                    {item.track.artists.map((artist) => artist.name).join(", ")}
+                  </p>
+                  <p>{item.track.name}</p>
+                  {item.track.album.images && (
+                    <img src={item.track.album.images[0]?.href} alt="" />
+                  )}
+                  {item.track.preview_url && (
+                    <audio controls={true}>
+                      <source src={item.track.preview_url} type="audio/mpeg" />
+                    </audio>
+                  )}
+                </li>
+              );
+            })}
+        </ul>
       </div>
     </>
   );
