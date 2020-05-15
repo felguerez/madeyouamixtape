215b25b5576b91abd7a7ec5655fb63541214cd39
Antonio Felguerez <antoniof@spotify.com>
2020-05-15 19:55:58 -0400

    Remove dumb util

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,18 +1,1 @@
-import Router from "next/router";
-
 export const serialize = (values: any) => JSON.parse(JSON.stringify(values));
-
-export function msToTime(duration: number) {
-  // @ts-ignore
-  var milliseconds = parseInt((duration % 1000) / 100),
-    seconds = Math.floor((duration / 1000) % 60),
-    minutes = Math.floor((duration / (1000 * 60)) % 60),
-    hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
-
-  hours = hours < 10 ? "0" + hours : hours;
-  minutes = minutes < 10 ? "0" + minutes : minutes;
-  seconds = seconds < 10 ? "0" + seconds : seconds;
-
-  return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
-}
-console.log(msToTime(300000));
