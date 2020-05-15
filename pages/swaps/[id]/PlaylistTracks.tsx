65f3e782d9068753136eb876d8f789900182063a
Antonio Felguerez <antoniof@spotify.com>
2020-05-15 19:17:48 -0400

    Style playlist

--- pages/swaps/[id]/PlaylistTracks.tsx
+++ pages/swaps/[id]/PlaylistTracks.tsx
@@ -4,6 +4,7 @@ import { Card, CopyContainer } from "../../../shared/styles";
 import styled from "@emotion/styled";
 import { css } from "@emotion/core";
 import { msToTime } from "../../../lib/utils";
+import ms from "ms";
 
 const PlaylistTracks = ({ playlist }) => {
   if (!playlist) {
@@ -11,53 +12,53 @@ const PlaylistTracks = ({ playlist }) => {
   }
 
   return (
-    <CopyContainer>
-      <TrackList>
-        {playlist.tracks &&
-          playlist.tracks.items.map((item) => {
-            console.log("item.track:", item.track);
-            return (
-              <Track key={item.id}>
-                {item.track.album.images && (
-                  <CoverArt src={item.track.album.images[0]?.url} alt="" />
-                )}
-                <TrackMetadata>
-                  <p>
-                    <strong>
-                      {item.track.artists
-                        .map((artist) => artist.name)
-                        .join(", ")}
-                    </strong>
-                    <span>
-                      <Bullet>&bull;</Bullet>
-                      {item.track.name}
-                    </span>
-                  </p>
-                  <Details>
-                    <Album>{item.track.album.name}</Album>{" "}
-                    {msToTime(item.track.duration_ms)}
-                  </Details>
-                </TrackMetadata>
-                {item.track.preview_url && (
-                  <audio>
-                    <source src={item.track.preview_url} type="audio/mpeg" />
-                  </audio>
-                )}
-              </Track>
-            );
-          })}
-      </TrackList>
-    </CopyContainer>
+    <TrackList>
+      {playlist.tracks &&
+        playlist.tracks.items.map((item) => {
+          console.log("item.track:", item.track);
+          return (
+            <Track key={item.id}>
+              {item.track.album.images && (
+                <CoverArt src={item.track.album.images[0]?.url} alt="" />
+              )}
+              <TrackMetadata>
+                <p>
+                  <strong>
+                    {item.track.artists.map((artist) => artist.name).join(", ")}
+                  </strong>
+                  <span>
+                    <Bullet>&bull;</Bullet>
+                    {item.track.name}
+                  </span>
+                </p>
+                <Details>
+                  <AlbumName>{item.track.album.name}</AlbumName>{" "}
+                  <Duration>
+                    {ms(item.track.duration_ms, { long: true })}
+                  </Duration>
+                </Details>
+              </TrackMetadata>
+              {item.track.preview_url && (
+                <audio>
+                  <source src={item.track.preview_url} type="audio/mpeg" />
+                </audio>
+              )}
+            </Track>
+          );
+        })}
+    </TrackList>
   );
 };
 
 const TrackList = styled.div`
-  padding-left: 0;
+  padding: 1rem;
   list-style: none;
 `;
 
 const Bullet = styled.span`
-  margin: 0 0.25rem;
+  margin: 0 0.5rem;
+  font-size: 0.75rem;
+  vertical-align: middle;
 `;
 
 const TrackMetadata = styled.div`
@@ -72,6 +73,9 @@ const TrackMetadata = styled.div`
 const Details = styled.p`
   color: rgb(179, 179, 179);
   font-size: 0.75rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
 `;
 
 const CoverArt = styled.img`
@@ -87,8 +91,14 @@ const Track = styled.li`
   }
 `;
 
-const Album = styled.strong`
+const AlbumName = styled.strong`
   margin-right: 1rem;
+  margin-bottom: auto;
+`;
+
+const Duration = styled.span`
+  margin-top: 0.5rem;
+  display: inline-block;
 `;
 
 export default PlaylistTracks;
